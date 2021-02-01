0a7fa2e86af9a00559ba1577daf3f4d3a2409836
kostyak127 <kostyak127@gmail.com>
2021-02-01 22:24:01 +0500

    config

--- db/config.py
+++ db/config.py
@@ -8,6 +8,6 @@ class PostgreSQLConfig:
     name = os.getenv('POSTGRES_DB', 'backendnapoleonit')
     user = os.getenv('POSTGRES_USER', 'admin')
     password = os.getenv('POSTGRES_PASSWORD', 'auf')
-    host = os.getenv('POSTGRES_HOST', 'localhost')
+    host = os.getenv('POSTGRES_HOST', 'backendnapoleonit-db')
     port = os.getenv('POSTGRES_PORT', '5432')
     url = rf'postgresql+psycopg2://{user}:{password}@{host}:{port}/{name}'
